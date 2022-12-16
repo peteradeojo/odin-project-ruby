f50ca330f9cdb1f16315e8a0de26f234b3eea27e
Peter Ade-Ojo <adeojopeter@gmail.com>
2022-12-16 01:00:16 +0100

    Project Update: Bubble sort

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,20 +1,14 @@
 def bubble_sort(array)
   array.each_with_index do
     array.each_with_index do |item, index|
+      next unless index + 1 < array.length && (item >= array[index + 1])
 
-      if index + 1 < array.length
-        
-        unless item < array.dig(index+1)
-          a = item
-          b = array.dig(index+1)
-          array[index] = b
-          array[index + 1] = a
-        end
-
-      end
-
+      a = item
+      b = array[index + 1]
+      array[index] = b
+      array[index + 1] = a
     end
   end
 end
 
-puts bubble_sort([24,4,3,78,2,0,2])
+puts bubble_sort([24, 4, 3, 78, 2, 0, 2])
